875c8882f59dd41280fa700d794ebeef0a8ec487
m.demidenko <m.demidenko@tbank.ru>
2026-01-19 21:30:26 +0300

    add code for swagger

--- docs/docs.go
+++ docs/docs.go
@@ -591,8 +591,8 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
-	BasePath:         "/api",
+	Host:             "127.0.0.1:8080",
+	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Monitoring Platform API",
 	Description:      "API для отправки уведомлений через Telegram",
